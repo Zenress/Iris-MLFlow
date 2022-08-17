d85ab6c649f114cef98f7d52c11031caaf3da0de
Shano <xshfo1806@dsb.dk>
2022-08-17 15:24:36 +0200

    Progress on the process step, ingest_test used for testing ideas

---
+++ tests/ingest_test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("data/irisdata_raw.csv",header=None, names= ["sepal length", "sepal width", "petal length", "petal width", "class"])
+shuffled_df = df.sample(frac=1, random_state=123)
+print(df.head())
+print(shuffled_df.head())
+shuffled_df.reset_index(drop=True,inplace=True)
+print(shuffled_df.head())
