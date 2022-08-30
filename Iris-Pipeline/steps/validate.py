f08052549343b0b6c62e6917875fb60c0ac86676
Zenress <hotrockgaming@gmail.com>
2022-08-30 16:06:32 +0200

    Good progress on the validate.py step

--- Iris-Pipeline/steps/validate.py
+++ Iris-Pipeline/steps/validate.py
@@ -1,3 +1,34 @@
 """
 Code for validating model performance using validation set that's derived from dataset
 """
+import os
+import mlflow
+import click
+import yaml
+import pandas as pd
+import os
+
+CONFIG_PATH = "../configuration/config.yaml"
+
+@click.command()
+@click.option("--process_run_id")
+def task(process_run_id):
+    """_summary_
+
+    Args:
+        process_run_id (_type_): _description_
+    """
+    with mlflow.start_run() as mlrun:
+        with open(CONFIG_PATH, "r", encoding="UTF-8") as ymlfile:
+            cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            
+        process_run = mlflow.tracking.MlflowClient().get_run(process_run_id)
+        
+        validate_path = os.path.join(process_run.info.artifact_uri, "validate_data.csv")
+        validate_df = pd.read_csv(validate_path)
+        
+        X_validate = validate_df[cfg["features"].keys()]
+        y_validate = validate_df[cfg["label_name"]]
+        
+if __name__ == "__main__":
+    task()
