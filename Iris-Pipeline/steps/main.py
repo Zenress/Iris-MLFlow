5af1cd5c607c6d83fc6fe0b5e49489e421e0369d
Zenress <hotrockgaming@gmail.com>
2022-08-30 15:53:41 +0200

    removed unnecessary OS library, removed unnecessary get_run line

--- Iris-Pipeline/steps/main.py
+++ Iris-Pipeline/steps/main.py
@@ -1,27 +1,22 @@
 """
 Here we orchestrate the steps that should be run
 """
-import os
 import mlflow
 
 def workflow():
     with mlflow.start_run() as active_run:
         print('Launching Ingest Step')
         ingest_run = mlflow.run("steps/","ingest.py", env_manager="local")
-        ingest_run = mlflow.tracking.MlflowClient().get_run(ingest_run.run_id)
-        dataset_path = os.path.join(ingest_run.info.artifact_uri, "irisdata_raw.csv")
-        
-        print(f"\n \n {dataset_path}")
         
         #Current parameters: dataframe
         print('Launching Data Processing step')
-        process_run = mlflow.run("steps/","process.py", parameters={"dataset_path": dataset_path}, env_manager="local")
+        process_run = mlflow.run("steps/","process.py", parameters={"dataset_run_id": ingest_run.run_id}, env_manager="local")
         
         print("Launching Training step")
         training_run = mlflow.run("steps/","train.py",parameters={"process_run_id": process_run.run_id}, env_manager="local")
         
         print("Launching Validatiom step")
-        
+        validate_run = mlflow.run("steps/","validate.py", parameters={"process_run_id": process_run.run_id}, env_manager="local")
         
 if __name__ == "__main__":
     workflow()
