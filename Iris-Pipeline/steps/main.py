22750e6169c6a2a10f846510c8fbdd887d25d6d0
Zenress <hotrockgaming@gmail.com>
2022-09-02 14:59:20 +0200

    stacked mlflow.run functions, added click library to access the graphs parameter from MLproject

--- Iris-Pipeline/steps/main.py
+++ Iris-Pipeline/steps/main.py
@@ -2,20 +2,58 @@
 Here we orchestrate the steps that should be run
 """
 import mlflow
+import click
 
-def workflow():
+CONFIG_PATH = "../configuration/config.yaml"
+
+
+@click.command()
+@click.option("--graphs")
+def workflow(graphs):
     with mlflow.start_run() as active_run:
-        print('Launching Ingest Step')
-        ingest_run = mlflow.run("steps/","ingest.py", env_manager="local")
-        
-        print('Launching Data Processing step')
-        process_run = mlflow.run("steps/","process.py", parameters={"dataset_run_id": ingest_run.run_id}, env_manager="local")
-        
+        print("Launching Ingest Step")
+        ingest_run = mlflow.run(
+            "steps/",
+            "ingest.py",
+            parameters={"config_path": CONFIG_PATH},
+            env_manager="local"
+        )
+
+        print("Launching Data Processing step")
+        process_run = mlflow.run(
+            "steps/",
+            "process.py",
+            parameters={
+                "dataset_run_id": ingest_run.run_id,
+                "config_path": CONFIG_PATH
+            },
+            env_manager="local",
+        )
+
         print("Launching Training step")
-        training_run = mlflow.run("steps/","train.py",parameters={"process_run_id": process_run.run_id}, env_manager="local")
-        
+        training_run = mlflow.run(
+            "steps/",
+            "train.py",
+            parameters={
+                "process_run_id": process_run.run_id,
+                "graphs": graphs,
+                "config_path": CONFIG_PATH
+            },
+            env_manager="local",
+        )
+
         print("Launching Validatiom step")
-        validate_run = mlflow.run("steps/","validate.py", parameters={"process_run_id": process_run.run_id, "train_run_id": training_run.run_id}, env_manager="local")
-        
+        validate_run = mlflow.run(
+            "steps/",
+            "validate.py",
+            parameters={
+                "process_run_id": process_run.run_id,
+                "train_run_id": training_run.run_id,
+                "config_path": CONFIG_PATH
+            },
+            env_manager="local",
+        )
+
+
 if __name__ == "__main__":
     workflow()
