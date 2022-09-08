01354a16c6a3d8e2b1d56ec38999e5856808cf58
Shano <xshfo1806@dsb.dk>
2022-09-08 11:14:29 +0200

    partway through using json to deploy the model

--- Iris-Pipeline/steps/validate.py
+++ Iris-Pipeline/steps/validate.py
@@ -11,6 +11,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 import json
 
+JSON_PATH = "Iris-Pipeline/models/"
 
 def compare_models(
     deployed_model: DecisionTreeClassifier,
@@ -30,8 +31,14 @@ def compare_models(
         json_name (str): _description_
         model_name (str): _description_
     """
-    deployed_model_score = metrics.accuracy_score(y_validate, deployed_model.predict(X_validate))
-    current_model_score = metrics.accuracy_score(y_validate, current_model.predict(X_validate))
+    deployed_model_score = metrics.accuracy_score(
+        y_validate,
+        deployed_model.predict(X_validate)
+        )
+    current_model_score = metrics.accuracy_score(
+        y_validate,
+        current_model.predict(X_validate)
+        )
     
     if deployed_model_score < current_model_score:
         deployed_cross_scores = cross_val_score(
@@ -58,7 +65,7 @@ def compare_models(
                 
             json_object['name'] = model_name
             
-            with open("iris-pipeline/models/deployed_model.json", "w") as file:
+            with open(json_name, "w") as file:
                 file.write(json_object)
         
 def evaluate(
@@ -110,19 +117,25 @@ def task(process_run_id, train_run_id, config_path):
             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
             
         model_version = train_run_id[:5]    
-            
+        json_name = Path(JSON_PATH, cfg["deployment_json_name"])
+         
         process_run = mlflow.tracking.MlflowClient().get_run(process_run_id)
         train_run = mlflow.tracking.MlflowClient().get_run(train_run_id)
         
         validate_path = Path(process_run.info.artifact_uri, "validate_data.csv")
         validate_df = pd.read_csv(validate_path)
         
+        with open(json_name, 'r') as openfile:
+            json_object = json.load(openfile)
+        
         model_name = cfg["model_name"] + f"-{model_version}"
         model_path = f"runs:/{train_run_id}/{model_name}"
+        deployed_path = f"..models/{json_object['name']}"
         
         print(str(model_path))
         
         dtc_model = mlflow.sklearn.load_model(str(model_path))
+        deployed_model = mlflow.sklearn.load_model(str(deployed_path))
         
         X_validate = validate_df[cfg["features"].keys()]
         y_validate = validate_df[cfg["label_name"]]
@@ -133,7 +146,14 @@ def task(process_run_id, train_run_id, config_path):
             y_validate=y_validate
             )
         
-        compare_models()
+        compare_models(
+            deployed_model=deployed_model,
+            current_model=dtc_model,
+            X_validate=X_validate,
+            y_validate=y_validate,
+            json_name=json_name,
+            model_name=model_name
+            )
         
 if __name__ == "__main__":
     task()
