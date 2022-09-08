8db6b3a7af498501cc6e5500b047c63566b27c5e
Zenress <hotrockgaming@gmail.com>
2022-09-08 15:45:29 +0200

    Got the json file to work correctly with model deployment, updated some variable names and completed the docstrings

--- Iris-Pipeline/steps/validate.py
+++ Iris-Pipeline/steps/validate.py
@@ -11,25 +11,32 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 import json
 
-JSON_PATH = "Iris-Pipeline/models/"
+JSON_PATH = "../models/"
 
 def compare_models(
     deployed_model: DecisionTreeClassifier,
     current_model: DecisionTreeClassifier,
     X_validate: pd.Series,
     y_validate: pd.Series,
-    json_name: str,
+    json_path: str,
     model_name: str,
     ) -> None:
-    """_summary_
+    """
+    Compare 2 models evaluation metrics against eachother to find the best one
+    
+    Compares the 2 models evaluation metrics against eachother,
+    to find out which one has better performance. 
+    There is the current model which is the one that was trained in this mlflow run,
+    then there is the deployed model which is the currently deployed model.
 
     Args:
-        deployed_model (DecisionTreeClassifier): _description_
-        current_model (DecisionTreeClassifier): _description_
-        X_validate (pd.Series): _description_
-        y_validate (pd.Series): _description_
-        json_name (str): _description_
-        model_name (str): _description_
+        deployed_model (DecisionTreeClassifier): the model that is used for deployment,
+            it's assigned in the json file
+        current_model (DecisionTreeClassifier): the model that was trained in the current mlflow run
+        X_validate (pd.Series): feature columns from the validation dataset
+        y_validate (pd.Series): label column from the validation dataset
+        json_path (str): the path to the json file used for deployment configuration
+        model_name (str): name of the model trained in this mlflow run
     """
     deployed_model_score = metrics.accuracy_score(
         y_validate,
@@ -42,16 +49,16 @@ def compare_models(
     
     if deployed_model_score < current_model_score:
         deployed_cross_scores = cross_val_score(
-        estimator=deployed_model,
-        X=X_validate,
-        y=y_validate,
-        )
+            estimator=deployed_model,
+            X=X_validate,
+            y=y_validate,
+            )
         
         current_cross_scores = cross_val_score(
-        estimator=current_model,
-        X=X_validate,
-        y=y_validate,
-        )
+            estimator=current_model,
+            X=X_validate,
+            y=y_validate,
+            )
         
         improved_scores = []
         
@@ -59,13 +66,17 @@ def compare_models(
             if score < current_cross_scores[count]:
                 improved_scores.append(current_cross_scores)
         
-        if improved_scores > deployed_cross_scores:
-            with open(json_name, 'r') as openfile:
+        print(deployed_cross_scores)
+        print(current_cross_scores)
+        
+        if len(improved_scores) > len(deployed_cross_scores):
+            print("Improved scores better than deployed scores")
+            with open(json_path, 'r') as openfile:
                 json_object = json.load(openfile)
                 
             json_object['name'] = model_name
             
-            with open(json_name, "w") as file:
+            with open(json_path, "w") as file:
                 file.write(json_object)
         
 def evaluate(
@@ -74,7 +85,10 @@ def evaluate(
     y_validate: pd.Series
     ) -> None:
     """
-    Evaluate the validation 
+    Evaluate on the validation set
+    
+    Evaluates the model from the train step against the validation set,
+    to see if the performance is good with unknown data
     
     Args:
         model (DecisionTreeClassifier): trained decision tree classifier,
@@ -107,8 +121,14 @@ def evaluate(
 @click.option("--config_path")
 def task(process_run_id, train_run_id, config_path):
     """
-    validate model performance against deployed model
+    Validate model performance against deployed model
 
+    Orchestrates the validation of model performance. 
+    This is done by first evaluating the model using this function:
+        evaluate(): Evaluates the model using the validation dataset
+    Then it compares the model to the one that is currently deployed using this function:
+        compare_models(): Compares the deployed model metrics to the current model metrics
+    
     Args:
         process_run_id (str): run id that the process step generated
         train_run_id (str): run id that the train step generated
@@ -119,7 +139,7 @@ def task(process_run_id, train_run_id, config_path):
             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
             
         model_version = train_run_id[:5]    
-        json_name = Path(JSON_PATH, cfg["deployment_json_name"])
+        json_path = Path(JSON_PATH, cfg["deployment_json_name"])
          
         process_run = mlflow.tracking.MlflowClient().get_run(process_run_id)
         train_run = mlflow.tracking.MlflowClient().get_run(train_run_id)
@@ -127,14 +147,12 @@ def task(process_run_id, train_run_id, config_path):
         validate_path = Path(process_run.info.artifact_uri, "validate_data.csv")
         validate_df = pd.read_csv(validate_path)
         
-        with open(json_name, 'r') as openfile:
+        with open(json_path, 'r') as openfile:
             json_object = json.load(openfile)
         
         model_name = cfg["model_name"] + f"-{model_version}"
         model_path = f"runs:/{train_run_id}/{model_name}"
-        deployed_path = f"..models/{json_object['name']}"
-        
-        print(str(model_path))
+        deployed_path = f"../models/{json_object['name']}"
         
         dtc_model = mlflow.sklearn.load_model(str(model_path))
         deployed_model = mlflow.sklearn.load_model(str(deployed_path))
@@ -153,7 +171,7 @@ def task(process_run_id, train_run_id, config_path):
             current_model=dtc_model,
             X_validate=X_validate,
             y_validate=y_validate,
-            json_name=json_name,
+            json_path=json_path,
             model_name=model_name
             )
         
