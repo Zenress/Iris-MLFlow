4a7e9c16d64101abb7d22143ce667fd5141e3c22
Shano <xshfo1806@dsb.dk>
2022-09-13 08:42:14 +0200

    removed train_split seperate method as i only needed train and validate now. There was no need for an entire function for just one single method

--- Iris-Pipeline/steps/process.py
+++ Iris-Pipeline/steps/process.py
@@ -35,70 +35,6 @@ def label_encoding_method(
     return dataset_df
 
 
-def split_stratified_into_train_val_test(
-    df_input: pd.DataFrame,
-    stratify_colname: str,
-    frac_train: float,
-    frac_val: float,
-    frac_test: float,
-    random_state: int
-    ) -> pd.DataFrame:
-    '''
-    Split a Dataframe into 3 sets
-    
-    Splits a Pandas dataframe into three subsets (train, val, and test)
-    following fractional ratios provided by the user, where each subset is
-    stratified by the values in a specific column (that is, each subset has
-    the same relative frequency of the values in the column). It performs this
-    splitting by running train_test_split() twice.
-
-    Args:
-        df_input (pd.DataFrame): Input dataframe to be split.
-        stratify_colname (str): Name of the column used for stratification.
-            Usually this column would be for the label.
-        frac_train (float): fractional value to split the training set
-        frac_val (float): fractional value to split the validate set
-        frac_test (float): fractional value to split the test set
-            The ratios with which the dataframe will be split into train, val, and
-            test data. The values should be expressed as float fractions and should
-            sum to 1.0.
-        random_state (int): None, or RandomStateInstance
-
-    Returns:
-        df_train, df_val, df_test (pd.DataFrame):
-            Dataframes containing the three splits.
-    '''
-    if frac_train + frac_val + frac_test != 1.0:
-        raise ValueError('fractions %f, %f, %f do not add up to 1.0' % \
-                         (frac_train, frac_val, frac_test))
-
-    if stratify_colname not in df_input.columns:
-        raise ValueError('%s is not a column in the dataframe' % (stratify_colname))
-
-    X = df_input # Contains all columns.
-    y = df_input[[stratify_colname]] # Dataframe of just the column on which to stratify.
-
-    # Split original dataframe into train and temp dataframes.
-    df_train, df_temp, y_train, y_temp = train_test_split(X,
-                                                          y,
-                                                          stratify=y,
-                                                          test_size=(1.0 - frac_train),
-                                                          random_state=random_state)
-
-    # Split the temp dataframe into val and test dataframes.
-    relative_frac_test = frac_test / (frac_val + frac_test)
-    df_val, df_test, y_val, y_test = train_test_split(df_temp,
-                                                      y_temp,
-                                                      stratify=y_temp,
-                                                      test_size=relative_frac_test,
-                                                      random_state=random_state)
-
-    #Makes sure the dataframes didn't lose or drop any records,
-    # during the above transformations
-    assert len(df_input) == len(df_train) + len(df_val) + len(df_test)
-
-    return df_train, df_val, df_test
-
 @click.command()
 @click.option("--dataset_run_id")
 @click.option("--config_path")
@@ -137,13 +73,11 @@ def task(dataset_run_id, config_path):
             label_name=cfg["label_name"]
             )
         
-        train_df, validate_df, test_df = split_stratified_into_train_val_test(
-            df_input=encoded_df,
-            stratify_colname=cfg["label_name"],
-            frac_train=0.6,
-            frac_val=0.15,
-            frac_test=0.25,
-            random_state=None
+        train_df, val_df = train_test_split(
+            encoded_df,
+            test_size=0.3,
+            train_size=0.7,
+            shuffle=True
             )
         
         print("Uploading train dataset: %s" % train_df.head(2))
@@ -151,14 +85,9 @@ def task(dataset_run_id, config_path):
         train_df.to_csv(train_path, index=False)
         mlflow.log_artifact(train_path)
         
-        print("Uploading train dataset: %s" % test_df.head(2))
-        test_path =  Path(cfg["dataset_path"], "test_data.csv")
-        test_df.to_csv(test_path, index=False)
-        mlflow.log_artifact(test_path)
-        
-        print("Uploading train dataset: %s" % validate_df.head(2))
+        print("Uploading train dataset: %s" % val_df.head(2))
         validate_path = Path(cfg["dataset_path"], "validate_data.csv")
-        validate_df.to_csv(validate_path, index=False)
+        val_df.to_csv(validate_path, index=False)
         mlflow.log_artifact(validate_path)
 
 if __name__ == '__main__':
