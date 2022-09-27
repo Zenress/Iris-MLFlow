dd6c6f9c6b48ef3fb29a034c9109a90e7bd86eb5
Shano <xshfo1806@dsb.dk>
2022-09-27 09:52:23 +0200

    removed unnecessary variable creation of csv_url and added set_tag for naming the specific step's run. The way i do requests are also revamped

--- Iris-Pipeline/steps/ingest.py
+++ Iris-Pipeline/steps/ingest.py
@@ -23,21 +23,18 @@ def load_file_as_dataframe(config_path) -> None:
         with open(config_path, "r", encoding="UTF-8") as ymlfile:
             cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-        csv_url = cfg["csv_url"]
+        mlflow.set_tag("mlflow.runName", "Data ingestion step")
 
         file_name = Path(cfg["dataset_path"], cfg["dataset_name"])
         
         if not file_name.exists():
             print("File doesn't exist")
             print("Downloading Dataset \n--")
-            try:
-                response_object = requests.get(url=csv_url, allow_redirects=True)
-            except requests.exceptions.RequestException as e: 
-                print(e)
-            
+            response_object = requests.get(url=cfg["csv_url"], allow_redirects=True)
+            response_object.raise_for_status()
             
             with open(file=file_name, mode="wb") as file:
-                file.write(response_object.content)
+                    file.write(response_object.content)
         else:
             print("File already exists")
             print("Using existing file")
