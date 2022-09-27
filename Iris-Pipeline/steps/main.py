66636ee7fbe8e82386d1d32e64eb7fac6078f32a
Shano <xshfo1806@dsb.dk>
2022-09-27 10:19:52 +0200

    Added better documentation for main.py

--- Iris-Pipeline/steps/main.py
+++ Iris-Pipeline/steps/main.py
@@ -10,15 +10,27 @@ CONFIG_PATH = "../configuration/config.yaml"
 @click.command()
 @click.option("--graphs")
 def workflow(graphs):
+    """Main function that orchestrates all the different steps of the pipeline
+    
+    This is the main function which orchestrates all the different pipeline steps
+    It also controls what the content of the parameters that are passed through are,
+    as well as what kind of environment is passed through to use for the pipeline
+    """
     with mlflow.start_run() as active_run:
+        mlflow.set_tag("mlflow.runName", "Iris Pipeline")
+        
+        # ---------------------------
+        # Data ingestion step
         print("Launching Ingest Step")
         ingest_run = mlflow.run(
             "steps/",
             "ingest.py",
             parameters={"config_path": CONFIG_PATH},
-            env_manager="local"
+            env_manager="local",
         )
-
+        
+        # ---------------------------
+        # Data processing step
         print("Launching Data Processing step")
         process_run = mlflow.run(
             "steps/",
@@ -30,6 +42,8 @@ def workflow(graphs):
             env_manager="local",
         )
 
+        # ---------------------------
+        # Model training step
         print("Launching Training step")
         training_run = mlflow.run(
             "steps/",
@@ -42,6 +56,8 @@ def workflow(graphs):
             env_manager="local",
         )
 
+        # ---------------------------
+        # Model validation step
         print("Launching Validatiom step")
         validate_run = mlflow.run(
             "steps/",
